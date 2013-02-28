28c48024a514ba0736e71dccd5a05890300a0b25
MiCHiLU <djmchl@gmail.com>
2013-02-28 12:13:44 +0900

    Rename

--- lib/Crypto/Signature/PKCS7_v1_5.py
+++ lib/Crypto/Signature/PKCS7_v1_5.py
@@ -1,43 +1,23 @@
 # -*- coding: utf-8 -*-
-#
-#  Signature/PKCS1-v1_5.py : PKCS#1 v1.5
-#
-# ===================================================================
-# The contents of this file are dedicated to the public domain.  To
-# the extent that dedication to the public domain is not available,
-# everyone is granted a worldwide, perpetual, royalty-free,
-# non-exclusive license to exercise all rights associated with the
-# contents of this file for any purpose whatsoever.
-# No rights are reserved.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# ===================================================================
 
 """
-RSA digital signature protocol according to PKCS#1 v1.5
+RSA digital signature protocol according to PKCS#7 v1.5
 
-See RFC3447__ or the `original RSA Labs specification`__.
+See RFC2313__ or the `original RSA Labs specification`__.
 
-This scheme is more properly called ``RSASSA-PKCS1-v1_5``.
+This scheme is more properly called ``RSASSA-PKCS7-v1_5``.
 
 For example, a sender may authenticate a message using SHA-1 like
 this:
 
-        >>> from Crypto.Signature import PKCS1_v1_5
+        >>> from Crypto.Signature import PKCS7_v1_5
         >>> from Crypto.Hash import SHA
         >>> from Crypto.PublicKey import RSA
         >>>
         >>> message = 'To be signed'
         >>> key = RSA.importKey(open('privkey.der').read())
         >>> h = SHA.new(message)
-        >>> signer = PKCS1_v1_5.new(key)
+        >>> signer = PKCS7_v1_5.new(key)
         >>> signature = signer.sign(h)
 
 At the receiver side, verification can be done using the public part of
@@ -45,7 +25,7 @@ the RSA key:
 
         >>> key = RSA.importKey(open('pubkey.der').read())
         >>> h = SHA.new(message)
-        >>> verifier = PKCS1_v1_5.new(key)
+        >>> verifier = PKCS7_v1_5.new(key)
         >>> if verifier.verify(h, signature):
         >>>    print "The signature is authentic."
         >>> else:
@@ -53,24 +33,24 @@ the RSA key:
 
 :undocumented: __revision__, __package__
 
-.. __: http://www.ietf.org/rfc/rfc3447.txt
-.. __: http://www.rsa.com/rsalabs/node.asp?id=2125
+.. __: http://www.ietf.org/rfc/rfc2313.txt
+.. __: http://www.rsa.com/rsalabs/node.asp?id=2129
 """
 
 __revision__ = "$Id$"
-__all__ = [ 'new', 'PKCS115_SigScheme' ]
+__all__ = [ 'new', 'PKCS715_SigScheme' ]
 
 import Crypto.Util.number
 from Crypto.Util.number import ceil_div
 from Crypto.Util.asn1 import DerSequence, DerNull, DerOctetString
 from Crypto.Util.py3compat import *
 
-class PKCS115_SigScheme:
-    """This signature scheme can perform PKCS#1 v1.5 RSA signature or verification."""
+class PKCS715_SigScheme:
+    """This signature scheme can perform PKCS#7 v1.5 RSA signature or verification."""
 
     def __init__(self, key):
-        """Initialize this PKCS#1 v1.5 signature scheme object.
-        
+        """Initialize this PKCS#7 v1.5 signature scheme object.
+
         :Parameters:
          key : an RSA key object
           If a private half is given, both signature and verification are possible.
@@ -83,16 +63,16 @@ class PKCS115_SigScheme:
         return self._key.has_private()
 
     def sign(self, mhash):
-        """Produce the PKCS#1 v1.5 signature of a message.
-    
-        This function is named ``RSASSA-PKCS1-V1_5-SIGN``, and is specified in
-        section 8.2.1 of RFC3447.
-    
+        """Produce the PKCS#7 v1.5 signature of a message.
+
+        This function is named ``RSASSA-PKCS7-V1_5-SIGN``, and is specified in
+        RFC2313.
+
         :Parameters:
          mhash : hash object
                 The hash that was carried out over the message. This is an object
                 belonging to the `Crypto.Hash` module.
-    
+
         :Return: The signature encoded as a string.
         :Raise ValueError:
             If the RSA key length is not sufficiently long to deal with the given
@@ -101,43 +81,43 @@ class PKCS115_SigScheme:
             If the RSA key has no private half.
         """
         # TODO: Verify the key is RSA
-    
+
         # See 8.2.1 in RFC3447
         modBits = Crypto.Util.number.size(self._key.n)
         k = ceil_div(modBits,8) # Convert from bits to bytes
-    
+
         # Step 1
-        em = EMSA_PKCS1_V1_5_ENCODE(mhash, k)
+        em = PKCS7_V1_5_ENCODE(mhash, k)
         # Step 2a (OS2IP) and 2b (RSASP1)
         m = self._key.decrypt(em)
         # Step 2c (I2OSP)
         S = bchr(0x00)*(k-len(m)) + m
         return S
-    
+
     def verify(self, mhash, S):
         """Verify that a certain PKCS#1 v1.5 signature is authentic.
-    
+
         This function checks if the party holding the private half of the key
         really signed the message.
-    
+
         This function is named ``RSASSA-PKCS1-V1_5-VERIFY``, and is specified in
         section 8.2.2 of RFC3447.
-    
+
         :Parameters:
          mhash : hash object
                 The hash that was carried out over the message. This is an object
                 belonging to the `Crypto.Hash` module.
          S : string
                 The signature that needs to be validated.
-    
+
         :Return: True if verification is correct. False otherwise.
         """
         # TODO: Verify the key is RSA
-    
+
         # See 8.2.2 in RFC3447
         modBits = Crypto.Util.number.size(self._key.n)
         k = ceil_div(modBits,8) # Convert from bits to bytes
-    
+
         # Step 1
         if len(S) != k:
             return 0
@@ -157,9 +137,9 @@ class PKCS115_SigScheme:
         # By comparing the full encodings (as opposed to checking each
         # of its components one at a time) we avoid attacks to the padding
         # scheme like Bleichenbacher's (see http://www.mail-archive.com/cryptography@metzdowd.com/msg06537).
-        # 
+        #
         return em1==em2
-    
+
 def EMSA_PKCS1_V1_5_ENCODE(hash, emLen):
     """
     Implement the ``EMSA-PKCS1-V1_5-ENCODE`` function, as defined
@@ -223,8 +203,8 @@ def EMSA_PKCS1_V1_5_ENCODE(hash, emLen):
     return b("\x00\x01") + PS + bchr(0x00) + digestInfo
 
 def new(key):
-    """Return a signature scheme object `PKCS115_SigScheme` that
-    can be used to perform PKCS#1 v1.5 signature or verification.
+    """Return a signature scheme object `PKCS715_SigScheme` that
+    can be used to perform PKCS#7 v1.5 signature or verification.
 
     :Parameters:
      key : RSA key object
@@ -232,5 +212,4 @@ def new(key):
       Signing is only possible if *key* is a private RSA key.
 
     """
-    return PKCS115_SigScheme(key)
-
+    return PKCS715_SigScheme(key)
